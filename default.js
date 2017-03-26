8627c085b7679027f32cb6d55a5315aa53a3531f
Nishant Mor <nishantmor27@gmail.com>
2017-03-26 15:47:45 +0530

    Added link Access rights

--- default.js
+++ default.js
@@ -9,12 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var url = "http://localhost:3000/api/insert";
     http.open("POST" , url , true);
     http.setRequestHeader("Content-type" ,"application/json");
-    http.setRequestHeader("Access-Control-Allow-Credentials" , "true");
-
     var data = JSON.stringify({"url_name":tab.url});
     http.send(data);
     console.log(tab.url);
-    
+
 
     });
   });
